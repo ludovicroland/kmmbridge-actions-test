24602bc0301d74d24d01c2a08b26223caa9be8a1
runner <runner@Mac-1677856118995.local>
2023-03-03 15:12:53 +0000

    KMM SPM package release for O.1.3

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "todo"
-let remoteKotlinChecksum = "todo"
+let remoteKotlinUrl = "https://maven.pkg.github.com/ludovicroland/kmmbridge-actions-test/KMMBridget_Test/shared-kmmbridge/O.1.3/shared-kmmbridge-O.1.3.zip"
+let remoteKotlinChecksum = "798a53ca5a3ee02eb72f65ea050119085aed9ef68f0495f2036244a31f57a131"
 let packageName = "TEST_KMM_IOS"
 
 let package = Package(
